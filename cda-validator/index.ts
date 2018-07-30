46cb68b2a296c51bea8ac4fcc70afeadea4b2b58
Hugo <hfernandez@neuquen.gov.ar>
2018-07-30 19:45:37 -0300

    estructura basica cda-validator

---
+++ cda-validator/index.ts
@@ -0,0 +1,21 @@
+import { Microservice, MSRouter, Middleware } from './../bootstrap';
+let pkg = require('./package.json');
+
+let ms = new Microservice(pkg);
+
+const router = MSRouter();
+
+// Empezar a codear como loco!!!!!!!!!
+
+router.group('/example', (group) => {
+    group.use(Middleware.authenticate());
+    group.get('/', (_req, res) => {
+        res.json({msg: 'This is an example'});
+    });
+    group.get('/all', (_req, res) => {
+        res.json({msg: 'ALL'});
+    });
+});
+
+ms.add(router);
+ms.start();
