9c76dfe3703671487256684277e68027b5d64a84
Hugo <hfernandez@neuquen.gov.ar>
2019-02-22 10:59:31 -0300

    update del package

--- facturacion/index.ts
+++ facturacion/index.ts
@@ -15,13 +15,15 @@ const router = ms.router();
 router.group('/facturacion', (group) => {
     mongoose.connect(mongoDB.mongoDB_main.host, { useNewUrlParser: true });
     group.post('/facturar', async (req, res) => {
-        sql.close();
-        let pool = await sql.connect(SipsDBConfiguration);
-
-        let dtoFacturacion: IDtoFacturacion = req.body.data;
-        let factura = new Factura();
-
-        factura.facturar(pool, dtoFacturacion);
+        try {
+            sql.close();
+            let pool = await sql.connect(SipsDBConfiguration);
+            let dtoFacturacion: IDtoFacturacion = req.body.data;
+            let factura = new Factura();
+            factura.facturar(pool, dtoFacturacion);
+        } catch (e) {
+            console.log('ERROR: ', e);
+        }
     });
 });
 
