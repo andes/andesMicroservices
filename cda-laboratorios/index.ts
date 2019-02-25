9fca3b17b7adc074894a6835c51863e0e1a814c6
Hugo <hfernandez@neuquen.gov.ar>
2019-02-25 13:55:59 -0300

    quito console log

--- cda-laboratorios/index.ts
+++ cda-laboratorios/index.ts
@@ -12,7 +12,6 @@ router.group('/cda', (group) => {
     // group.use(Middleware.authenticate());
     group.post('/ejecutar', (req, res) => {
         res.send({ message: 'ok' });
-        console.log('entro al ws');
         const id = req.body.id;
         const webhookId = req.body.subscription;
         const event = req.body.event;
@@ -28,12 +27,9 @@ router.group('/cda', (group) => {
                 break;
         }
 
-        console.log('acaaa: ', paciente);
         // Esperamos el paciente desde una prestación.
         if (paciente) {
-            console.log('tengo paciente: ', paciente);
             queue.add(() => {
-                console.log('encolando....');
                 return importarDatos(paciente);
             });
         }
