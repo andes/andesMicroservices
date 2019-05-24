7aae4e35ee4d69ae66d257ae2ab53d5c2e45b0b3
Luis <luisparadawagner@gmail.com>
2019-05-24 10:45:45 -0300

    Se parametriza conceptId en sys_especialidad sips para Junìn

--- cda-validator/controller/queries/junin.ts
+++ cda-validator/controller/queries/junin.ts
@@ -12,24 +12,33 @@ function make(paciente: any) {
     const dni = paciente.documento;
 
     const query =
-        `select
-                consulta.idConsulta as id,
-                convert(varchar(max),391000013108) as prestacion,
-                efector.idEfector as idEfector,
-                consulta.fecha as fecha,
-                efector.codigoSisa as sisa,
-                pac.numeroDocumento as pacienteDocumento, pac.nombre as pacienteNombre, pac.apellido as pacienteApellido,
-                pac.fechaNacimiento as pacienteFechaNacimiento, sex.nombre as pacienteSexo,
-                prof.numeroDocumento as profesionalDocumento, prof.nombre as profesionalNombre, prof.apellido as profesionalApellido, prof.matricula as profesionalMatricula,
-                cie.CODIGO as cie10, consulta.informeConsulta as texto
-                from Sys_Paciente as pac
-                inner join sys_sexo as sex on sex.idSexo = pac.idSexo
-                inner join CON_Consulta as consulta on consulta.idPaciente = pac.idPaciente
-                inner join CON_ConsultaDiagnostico as consultaD on consultaD.idConsulta = consulta.idConsulta
-                inner join Sys_CIE10 as cie on consultaD.CODCIE10 = cie.ID
-                inner join Sys_Profesional as prof on consulta.idProfesional = prof.idProfesional
-                inner join Sys_Efector as efector on consulta.idEfector = efector.idEfector
-                where pac.numeroDocumento = '${dni}'`;
+        `SELECT  consulta.idConsulta AS id ,
+        CASE WHEN e.conceptId_snomed IS NOT NULL THEN e.conceptId_snomed
+             ELSE '11429006'
+        END AS prestacion ,
+        efector.idEfector AS idEfector ,
+        consulta.fecha AS fecha ,
+        efector.codigoSisa AS sisa ,
+        pac.numeroDocumento AS pacienteDocumento ,
+        pac.nombre AS pacienteNombre ,
+        pac.apellido AS pacienteApellido ,
+        pac.fechaNacimiento AS pacienteFechaNacimiento ,
+        sex.nombre AS pacienteSexo ,
+        prof.numeroDocumento AS profesionalDocumento ,
+        prof.nombre AS profesionalNombre ,
+        prof.apellido AS profesionalApellido ,
+        prof.matricula AS profesionalMatricula ,
+        cie.CODIGO AS cie10 ,
+        consulta.informeConsulta AS texto
+FROM    Sys_Paciente AS pac
+        INNER JOIN Sys_Sexo AS sex ON sex.idSexo = pac.idSexo
+        INNER JOIN CON_Consulta AS consulta ON consulta.idPaciente = pac.idPaciente
+        INNER JOIN CON_ConsultaDiagnostico AS consultaD ON consultaD.idConsulta = consulta.idConsulta
+        INNER JOIN Sys_CIE10 AS cie ON consultaD.CODCIE10 = cie.ID
+        INNER JOIN Sys_Profesional AS prof ON consulta.idProfesional = prof.idProfesional
+        INNER JOIN Sys_Efector AS efector ON consulta.idEfector = efector.idEfector
+        INNER JOIN dbo.Sys_Especialidad AS e ON e.idEspecialidad = consulta.idEspecialidad
+WHERE   pac.numeroDocumento = '${dni}'`;
 
     return {
         connectionString,
