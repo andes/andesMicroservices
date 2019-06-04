cc0aa5f3cde0970b1e6f73f170b1695bace3ce8d
Nahir <nahirsaddi@gmail.com>
2019-06-04 11:15:47 -0300

    autenticacion gestion

--- appGestion/index.ts
+++ appGestion/index.ts
@@ -1,20 +1,34 @@
-import { Microservice, Middleware } from '@andes/bootstrap';
+import { Microservice } from '@andes/bootstrap';
 import { recuperaDatos } from './controller/recuperaDatos';
+
+import { postLogin } from './service/login';
 let pkg = require('./package.json');
 let ms = new Microservice(pkg);
 const router = ms.router();
 
 router.group('/mobile', (group) => {
-    // group.use(Middleware.authenticate());
-    group.get('/migrar', async (_req, res) => {
+    group.get('/migrar', async (_req: any, res) => {
         let lista = [];
-        const registros = await recuperaDatos();
-        if (registros.length > 0) {
-            registros.map(async registro => {
-                lista = lista.concat(registro);
-            });
+        let usuario = _req.query.usuario;
+        let login;
+        try {
+            login = await postLogin(usuario);
+            if (login && login.estado >= 200 && login.estado < 300) {
+                // migrar datos
+                const registros = await recuperaDatos();
+                if (registros.length > 0) {
+                    registros.map(async registro => {
+                        lista = lista.concat(registro);
+                    });
+                }
+            }
+            // devuelve un arreglo vacio en caso que no realizar la migración de datos
+            res.json(lista);
+        } catch (ex) {
+            return ex;
         }
-        res.json(lista);
+
+
     });
 });
 
