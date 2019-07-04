7aedcdf583de8d0d3a8f5937527a6d71178d0de9
Luis <luisparadawagner@gmail.com>
2019-07-04 14:58:37 -0300

    Se corrige bug en formación de profesional

--- facturacion/facturar/dto-facturacion.ts
+++ facturacion/facturar/dto-facturacion.ts
@@ -43,13 +43,7 @@ export async function facturacionAutomatica(prestacion: any) {
                 nombre: datosFactura[x].profesional.nombre,
                 apellido: datosFactura[x].profesional.apellido,
                 dni: datosFactura[x].profesional.dni,
-                formacionGrado: (datosFactura[x].profesional.formacionGrado.length > 0) ? datosFactura[x].profesional.formacionGrado.find((element) => {
-                    if (element.profesion.nombre === 'MEDICO') {
-                        return element.profesion.nombre.toLowerCase();
-                    } else {
-                        return null;
-                    }
-                }) : null
+                formacionGrado: (datosFactura[x].profesional.formacionGrado.length > 0) ? datosFactura[x].profesional.formacionGrado.find(f => f.profesion.nombre).profesion.nombre.toLowerCase() : null
             } : null,
             configAutomatica: datosFactura[x].configAutomatica
         };
