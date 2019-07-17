e77ad129489b88d621a2109d99e4c24e1a903d2a
Luis <luisparadawagner@gmail.com>
2019-07-17 09:56:20 -0300

    Se agrega hpn a constantes de hospitales en generación cda

--- cda-validator/constantes.ts
+++ cda-validator/constantes.ts
@@ -1,4 +1,3 @@
 export const efectores = [
-    // 'hpn', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet'
-    'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet'
+     'hpn', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet'
 ];
