9b0cc231ef86ecb7421c4618ce912e68536db880
Luis <luisparadawagner@gmail.com>
2019-08-28 14:35:11 -0300

    feat(CDA): Se generan cda para Rincón de los Sauces y Villa la Angostura

--- cda-validator/constantes.ts
+++ cda-validator/constantes.ts
@@ -1,3 +1,3 @@
 export const efectores = [
-     'hpn', 'heller', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet', 'senillosa', 'picun', 'piedra', 'chanar'
+     'hpn', 'heller', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet', 'senillosa', 'picun', 'piedra', 'chanar', 'rincon', 'villa_angostura'
 ];
