11fa29f73de03a053a5fa90d8e061a9f34de1a98
celeste <MCele>
2020-01-06 12:03:40 -0300

    bi-queries(feat):se conecta a BD ybmodifica getCSV

--- bi-queries/controller/queries.controller.ts
+++ bi-queries/controller/queries.controller.ts
@@ -1,163 +1,100 @@
-import { QuerySchema } from '../schemas/query';
-
-// import { ConsultasRepository } from '../repositories';
-import * as moment from 'moment';
-import { log } from '@andes/log';
-
-
-const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
-
-const MongoClient = require('mongodb').MongoClient;
 import { mongoDB } from './../config.private';
 
+const MongoClient = require('mongodb').MongoClient;
 const url = mongoDB.mongoDB_main.host;
 
+
 export async function getAllQueries() {
-  console.log("Entra a ALll Queries");
   let client = await MongoClient.connect(url);
-  let db = await client.db();
-
-  let cursor = await db.collection('queries').find({});
+  let cursor = await client.db().collection('queries').find({});
   let queries = await cursor.toArray();
-
+  // Close the connection
+  client.close();
   return queries;
 }
 
+// const palQuery = ['#match', '#and', '#gte', '#lte', '#exists', '#unwind', '#project', '#group', '#lookup', '#addFields', '#slice', '#in', '#arrayElemAt'];
 
 export async function descargarCSV(unaQuery) {
   let client = await MongoClient.connect(url);
   let db = await client.db();
-  
-  let datos: any = {
-    params: {
-      _id: "5de903a501ecb1b3367d7f51",
-      nombre: "Listado de Pacientes",
-      coleccion: "agenda",
-      // query: [{ '$match': { '$and': [{ 'createdAt': { '$gte': '@fechaInicio', '$lte': '@fechaFin' } }] } }, { '$addFields': { 'dire': { '$slice': ['$direccion', 0, 1] } } }, { '$unwind': '$dire' }, { '$project': { '_id': 1, 'nombre': 1, 'apellido': 1, 'documento': 1, 'sexo': 1, 'fechaNacimiento': 1, 'provincia': '$dire.ubicacion.provincia.nombre', 'localidad': '$dire.ubicacion.localidad.nombre', 'calle': '$dire.valor', 'pais': '$dire.ubicacion.pais.nombre' } }],
-      query: [
-        {
-          "$match": {
-            "$and": [{ "updatedAt": { "$gte": '@fechaInicio', "$lte": '@fechaFin' } }],
-            'profesionales.0': { "$exists": true }
-          }
-        },
-        { "$unwind": { "path": "$profesionales" } },
-        { "$group": { "_id": { "idProfesional": "$profesionales._id", "idAgenda": "$_id" } } },
-        { "$lookup": { "from": "profesional", "localField": "_id.idProfesional", "foreignField": "_id", "as": "dni" } },
-        { "$unwind": "$dni" },
-        { "$project": { "_id": 0, "idAgenda": "$_id.idAgenda", "idProfesional": "$_id.idProfesional", "documento": "$dni.documento" } }
-      ],
-      argumentos: [
-        {
-          key: "$gte",
-          label: "Fecha de Inicio",
-          param: "@fechaInicio",
-          tipo: "date",
-          componente: "FechaComponent",
-          nombre: "fechaInicio",
-          valor: "2017-11-01T15:56:51.785-03:00"
-        },
-        {
-          key: "$lte",
-          label: "Fecha de Fin",
-          param: "@fechaFin",
-          tipo: "date",
-          componente: "FechaComponent",
-          nombre: "fechaFin",
-          valor: "2019-12-19T15:56:51.785-03:00"
-        }
-      ]
-    }
-  };
+  var collection = db.collection(unaQuery.coleccion); // nombre de la coleccion
+  let pipelineSP = unaQuery.query;
 
-  var collection = db.collection(datos.params.coleccion); //name of db collection
+  pipelineSP = JSON.parse(JSON.stringify(pipelineSP).replace(/#/g, '$'));
 
-  let datosArgumentos = datos.params.argumentos;
-  console.log("Argumentos: ", datosArgumentos);
+  let datosArgumentos = unaQuery.argumentos;
 
-  let objeto = datos.params.query;
-
-  datosArgumentos.forEach((d: any) => {
-    console.log("Entra a datos Argumentos: ", d);
-    let index = datos.params.argumentos.findIndex((a: any) => a.param === d.param);
-    let replace;
-    // d.value = fechas[index].fecha;
-    console.log("D Value: ", d.valor);
-    switch (datos.params.argumentos[index].tipo) {
-      case 'date':
-        replace = parseDate(d.valor);
-        break;
-    }
-    datos.params.argumentos[index]['dato'] = replace;
-    console.log("Datata de cero: ", datos.params.argumentos[index]['dato']);
-    findValues(objeto, d.key, replace);
-  });
-
-  function parseDate(fecha: any) {
-    console.log("Entra a parseDate");
-    let x = Date.parse(fecha);
-    return new Date(x);
+  if (datosArgumentos) {
+    datosArgumentos.forEach((unArg: any) => {
+      // let index = datos.argumentos.findIndex((a: any) => a.param === d.param);
+      let replace = unArg.valor;
+      if (unArg.tipo === 'date') { replace = parseDate(unArg.valor); }
+      findValues(pipelineSP, unArg.key, unArg.param, replace);
+    });
   }
+  // console.log('objeto antes: ', JSON.stringify(unaQuery.query));
+  // console.log('objeto despues:', JSON.stringify(pipelineSP));
 
-  function findValues(obj: any, key: any, argumentos: any) {
-    console.log("Entra a findValues");
-    return findValuesHelper(obj, key, argumentos);
-  }
-
-  function findValuesHelper(obj: any, key: any, data: any) {
-    console.log("Entra a findValuesHelper: ", JSON.stringify(obj) + ' -- ' + key + ' -- ' + data);
-
-    if (!obj) { return; }
-
-    if (obj instanceof Array) {
-      console.log("Entra a Instance of");
-      for (let i in obj) {
-        findValuesHelper(obj[i], key, data);
-      }
-      // return;
-    }
-
-    if (obj[key]) {
-      console.log("Adentroooooo OBJ[KEYYY] ANtes: ", obj[key]);
-      obj[key] = data;
-      console.log("Adentroooooo OBJ[KEYYY] Despúes: ", obj[key]);
-    }
-    if ((typeof obj === 'object') && (obj !== null)) {
-      console.log("Entra a typeof oBJEcttt");
-      let children = Object.keys(obj);
-      console.log("Childrennnn: ", children);
-      if (children.length > 0) {
-        for (let i = 0; i < children.length; i++) {
-          findValuesHelper(obj[children[i]], key, data);
-        }
-      }
-    }
-    // return;
-  }
-
-  console.log('objeto despues', JSON.stringify(objeto));
-
-  var respuesta = await collection.aggregate(objeto);
-
+  var respuesta = await collection.aggregate(pipelineSP);
+  // console.log("Respuesta: ", respuesta);
   let pipe = await respuesta.toArray();
-  console.log("Pepepepe: ", pipe);
-
-  let csv = datos.params.coleccion + '.csv';
-
-  let someObject = pipe[0] // JSON array
-  let csvHeader = []
-  for (let key in someObject) {
+  client.close();
+  // generamos archivo CSV
+  const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+  let someObject = pipe[0];
+  let csvHeader = [];
+  for (let key in someObject) { // generamos el contenido del header: clave y nombre de las columnas del CSV
     csvHeader.push({ id: key, title: key });
   }
-
-  const csvStringifier = createCsvStringifier({
+  const csvStringifier = createCsvStringifier({ // se crea header
     header: csvHeader
   });
-
-  console.log(csvStringifier.getHeaderString());
-  // => 'NAME,LANGUAGE\n'
-  console.log(csvStringifier.stringifyRecords(pipe));
-  let capo = await csvStringifier.stringifyRecords(pipe);
-
-  return csvStringifier.stringifyRecords(pipe);
+  let ret = csvStringifier.getHeaderString() + await csvStringifier.stringifyRecords(pipe);
+  // console.log("CSV= ", ret);
+  return ret;
 }
+function parseDate(fecha: any) {
+  let x = Date.parse(fecha);
+  return new Date(x);
+}
+
+function findValues(obj: any, key: any, valorBusq: any, valorReemp: any) {
+  return findValuesHelper(obj, key, valorBusq, valorReemp);
+}
+
+function findValuesHelper(obj: any, key: any, valorBusq: any, valorReemp: any) {
+  if (!obj) { return; }
+  if (obj instanceof Array) {
+    for (let i in obj) {
+      // console.log("llama findHelper con i=", i, "y obj[i]", obj[i]);
+      findValuesHelper(obj[i], key, valorBusq, valorReemp);
+    }
+  }
+  else {
+    if (obj[key] === valorBusq) {
+      obj[key] = valorReemp;
+      // console.log("Entra OBJ [KEY]", valorBusq);
+    }
+    if ((typeof obj === 'object') && (obj !== null)) {
+      // console.log("Entra a typeof oBJEcttt");
+      let children = Object.keys(obj);
+
+      if (children.length > 0) {
+        for (let i = 0; i < children.length; i++) {
+          // console.log("Childrennnn: ", children, obj[children[i]]);
+          findValuesHelper(obj[children[i]], key, valorBusq, valorReemp);
+        }
+      }
+    }
+  }
+
+  // return;
+}
+// function convertirCadEspeciales(cadena: string) {
+//   let cadAux = cadena;
+//   palQuery.forEach(pal => {
+//     cadAux = JSON.parse(JSON.stringify(cadAux).replace(new RegExp(pal, 'g'), '$' + pal));
+//   });
+//   return cadAux;
+// }
--- bi-queries/index.ts
+++ bi-queries/index.ts
@@ -1,12 +1,10 @@
 import { Microservice } from '@andes/bootstrap';
 import { getAllQueries, descargarCSV } from './controller/queries.controller';
-import { mongoDB } from './config.private';
+import { mongoDB, logDatabase } from './config.private';
 
 let pkg = require('./package.json');
 let ms = new Microservice(pkg);
 const mongoose = require('mongoose');
-
-import { logDatabase } from './config.private';
 import { Connections, log } from '@andes/log';
 
 const router = ms.router();
@@ -14,11 +12,9 @@ const router = ms.router();
 router.group('/queries', (group) => {
     Connections.initialize(logDatabase.log.host, logDatabase.log.options);
     mongoose.connect(mongoDB.mongoDB_main.host, { useNewUrlParser: true });
-    console.log("Entra a CSV desde Api");
     group.post('/descargarCsv', async (req, res) => {
         let resQuery: any;
         try {
-            console.log("Entra a descargarrrrr CSV desde APIIIIIII");
             let event = req.body.event;
             const data = req.body.data;
             switch (event) {
@@ -26,19 +22,21 @@ router.group('/queries', (group) => {
                     resQuery = await getAllQueries();
                     break;
                 case 'queries:consultas:getCsv':
+
                     resQuery = await descargarCSV(data);
                     break;
                 default:
-                    console.log("Error");
-                    //await log(fakeRequestSql, 'queries:consultas:getQueries', null, ' / Origen query inválido', null);
+                    console.log('Error');
+                    // await log(logDatabase, 'queries:consultas:getQueries', null, ' / Origen query inválido', null);
                     break;
             }
         } catch (error) {
+            // return error;// ver error
             console.log("Error Catch: ", error);
-            //await  log(fakeRequestSql, 'queries:consultas:getQueries', null, ' / Origen query inválido', null, error);
+            // await log(logDatabase, 'queries:consultas:getQueries', null, ' / Origen query inválido', null, error);
         }
-        console.log("resQuery: ", resQuery);
-        res.json(JSON.stringify(resQuery));
+        // console.log("resQuery: ", resQuery);
+        res.json(resQuery);
     });
 });
 
