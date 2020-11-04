22cd89c7dfdde7937b431771d311c2f0c943d252
plammel <lammelpablo@gmail.com>
2020-11-04 11:23:46 -0300

    feat: CDAs COVID Lab Central

---
+++ cda-laboratorio-central/controller/import-labs.ts
@@ -0,0 +1,65 @@
+import { conSql } from '../config.private';
+import { log } from '@andes/log';
+import * as moment from 'moment';
+import * as sql from 'mssql';
+import * as operations from './operations';
+import * as fs from 'fs'
+
+const cota = 0.95;
+
+const connection = {
+    user: conSql.auth.user,
+    password: conSql.auth.password,
+    server: conSql.serverSql.server,
+    database: conSql.serverSql.database,
+    options: {
+        encrypt: true
+    }
+};
+
+export async function importarDatos(paciente) {
+    try {
+        const pool = await new sql.ConnectionPool(connection).connect();
+        let laboratorios: any = await operations.getEncabezados(pool, paciente.documento);
+        for (const lab of laboratorios.recordset) {
+            try {
+                const details: any = await operations.getDetalles(pool, lab.idProtocolo, lab.idEfector);
+                const organizacion: any = await operations.organizacionBySisaCode(lab.efectorCodSisa);
+
+                if (details.recordset) {
+                    const fecha = moment(lab.fecha, 'DD/MM/YYYY');
+
+                    const profesional = {
+                        nombre: lab.solicitante,
+                        apellido: '-' // Nombre y Apellido viene junto en los registros de laboratorio de SQL
+                    };
+
+                    fs.readFile((await operations.generarAdjunto(lab, details.recordset[0]) as string), async (err, data) => {
+                        if (err) throw err;
+                        const adjunto64 = 'data:application/pdf;base64,' + data.toString('base64');
+                        const dto = {
+                            id: lab.idProtocolo,
+                            organizacion: organizacion._id,
+                            fecha: fecha.toDate().toString(),
+                            tipoPrestacion: '4241000179101',
+                            paciente,
+                            confidencialidad: 'N',
+                            profesional,
+                            cie10: 'Z01.7',
+                            file: adjunto64,
+                            texto: details.recordset[0].resultado
+                        };
+                        await operations.postCDA(dto);
+                    });
+                }
+            } catch (error) {
+                log(operations.fakeRequest, 'microservices:integration:cda-labcentral', null, 'importarDatos:error', { error });
+            }
+        }
+        pool.close();
+        return true;
+    } catch (error) {
+        log(operations.fakeRequest, 'microservices:integration:cda-labcentral', null, 'importarDatos:error', { error });
+        return true;
+    }
+}
---
+++ cda-laboratorio-central/controller/operations.ts
@@ -0,0 +1,140 @@
+
+import * as sql from 'mssql';
+import { ANDES_HOST, ANDES_KEY, IP_LABCENTRAL } from '../config.private';
+import { log } from '@andes/log';
+import * as pdf from 'html-pdf';
+const got = require('got');
+const cache = {};
+
+export const fakeRequest = {
+    user: {
+        usuario: 'msLabCentral',
+        app: 'integracion-labcentral',
+        organizacion: 'sss'
+    },
+    ip: IP_LABCENTRAL,
+    connection: {
+        localAddress: ''
+    }
+}
+
+export async function organizacionBySisaCode(sisa) {
+    return new Promise( async (resolve, reject) => {
+        if (cache[sisa]) {
+            return resolve(cache[sisa]);
+        } else {
+            const url = `${ANDES_HOST}/core/tm/organizaciones?sisa=${sisa}&token=${ANDES_KEY}`;
+            const {error, statusCode, body} = await got(url, { responseType: 'json' });
+            if (error) {
+                log(fakeRequest, 'microservices:integration:cda-labcentral', null, 'postCDA:error', { error });
+            } else if (statusCode >= 200 && statusCode < 300) {
+                const orgs: any[] = body;
+                if (orgs && orgs.length) {
+                    cache[sisa] = {
+                        _id: orgs[0].id,
+                        nombre: orgs[0].nombre,
+                    };
+                    return resolve(cache[sisa]);
+                }
+            }
+            return reject(error || body);
+        }
+    });
+}
+
+
+export async function getEncabezados(pool, documento) {
+    const query = `select efector.codigoSisa as efectorCodSisa, efector.nombre as efector, encabezado.idEfector as idEfector, paciente.apellido as pacienteApellido, paciente.nombre as pacienteNombre, FORMAT(paciente.fechaNacimiento, 'dd/MM/yyyy') as fechaNacimiento, encabezado.sexo,  
+        paciente.numeroDocumento as numeroDocumento, FORMAT(encabezado.fechaTomaMuestra, 'dd/MM/yyyy') as fecha, encabezado.idProtocolo , solicitante.nombre as solicitante from LAB_Protocolo as encabezado  
+        inner join Sys_Efector as efector on encabezado.idEfector = efector.idEfector 
+        inner join Sys_Efector as solicitante on encabezado.idEfectorSolicitante = solicitante.idEfector 
+        inner join Sys_Paciente as paciente on encabezado.idPaciente = paciente.idPaciente 
+        where fechaRegistro > '2020-01-01' 
+        AND paciente.numeroDocumento = ${documento}`;
+    try {
+        return await new sql.Request(pool).query(query);
+    } catch (error) {
+        log(fakeRequest, 'microservices:integration:cda-labcentral', null, 'getEncabezados:error', { error, query });
+        return error;
+    } 
+}
+
+export async function getDetalles(pool, idProtocolo, idEfector) {
+    const query = `select _item.nombre, resultadoCar  as resultado, observaciones, FORMAT(detalle.fechaValida, 'dd/MM/yyyy') as fecha_validacion, usuarioValida.apellido as profesional_validacion
+        from LAB_DetalleProtocolo as detalle 
+        inner join LAB_Item as _item on _item.idItem = detalle.idItem
+        inner join Sys_Usuario as usuarioValida on usuarioValida.idUsuario = detalle.idUsuarioValida
+        where (detalle.idItem = 3174 or detalle.idItem = 3172) 
+        and detalle.idUsuarioValida <> 0
+        and detalle.idProtocolo = ${idProtocolo}`;
+    try {
+        return await new sql.Request(pool).query(query);
+    } catch (error) {
+        log(fakeRequest, 'microservices:integration:cda-labcentral', null, 'getDetalles:error', { error, query });
+        return error;
+    } 
+}
+
+export function postCDA(data: any) {
+    return new Promise( async (resolve: any, reject: any) => {
+        const url = `${ANDES_HOST}/modules/cda/create`;
+        const options = {
+            json: data,
+            headers: {
+                Authorization: `JWT ${ANDES_KEY}`
+            },
+            responseType: 'json'
+        };
+        const { error, statusCode, body } = await got.post(url, options);
+        if (error) {
+            log(fakeRequest, 'microservices:integration:cda-labcentral', null, 'postCDA:error', { error });
+        }
+        if (statusCode >= 200 && statusCode < 300) {
+            return resolve(body);
+        }
+        return resolve(error || body);
+    });
+}
+
+export async function generarAdjunto(cabecera, detalle) {
+    let html = `<!DOCTYPE html>
+        <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                <header>    
+                    ${detalle.nombre}
+                </header>
+                <section>
+                    <br/><br/>
+                    <label>Analizado por:</label> ${cabecera.efector}
+                    <br/><br/>
+                    <label>Solicitante:</label> ${cabecera.solicitante}
+                    <br/><br/>
+                    <label>Fecha toma muestra:</label> ${cabecera.fecha}
+                    <br/><br/>
+                    <label>Fecha validación resultado:</label> ${detalle.fecha_validacion}
+                    <br/><br/>
+                    <label>Paciente:</label> ${cabecera.pacienteApellido}, ${cabecera.pacienteNombre}
+                    <br/><br/>
+                    <label>DNI:</label> ${cabecera.numeroDocumento}
+                    <br/><br/>
+                    <label>Resultado:</label> ${detalle.resultado}
+                </section>
+                <footer></footer>
+            </body>
+        </html>`;
+
+    return new Promise((resolve, reject) => {
+        const options = { format: 'Letter', header: '3cm' };
+        pdf.create(html, options).toFile((error, file) => {
+            if (error) {
+                log(fakeRequest, 'microservices:integration:cda-labcentral', null, 'postCDA:error', { error });
+                return error;
+            }
+            resolve(file.filename);
+        });
+    });
+}
