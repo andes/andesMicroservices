0053dafc8c1fc828e788d8b38fcf5d7939529461
condorpiedra <natalia.huenchuman@gmail.com>
2021-01-29 13:52:25 -0300

    fix(cda-nomivac): cambia formato de response (#115)

--- cda-nomivac/controller/sisa.ts
+++ cda-nomivac/controller/sisa.ts
@@ -2,6 +2,7 @@ import { log } from '@andes/log';
 import * as operations from '../service/nomivacCDA';
 import { organizacionId } from '../config.private';
 import { sisaVacunas } from '../service/nomivacWSsisa';
+import * as moment from 'moment';
 /**
  * Actualiza las vacunas de un paciente de ANDES usando el webservice de NOMIVAC
  *
@@ -11,46 +12,50 @@ import { sisaVacunas } from '../service/nomivacWSsisa';
 export async function getVacunas(paciente) {
     if (paciente && paciente.documento) {
         try {
-            const vacunas: any = await sisaVacunas(paciente);
+            const listado_vacunas: any = await sisaVacunas(paciente);
+            if (listado_vacunas && listado_vacunas.length > 0) {
+                let promesas = [];
+                const vacunas = listado_vacunas[0].listado;
+                for (let i = 0; i < vacunas.length; i++) {
+                    let texto = vacunas[i].origenNombre ? `Organización:  ${vacunas[i].origenNombre} / ` : '';
+                    const dto = {
+                        id: vacunas[i].idSniAplicacion.toString(), // codigo SISA de la vacuna
+                        organizacion: organizacionId,
+                        fecha: moment(vacunas[i].fechaAplicacion).startOf('day').toDate(),  // Fecha de aplicación de la dosis de vacuna.
+                        tipoPrestacion: '33879002', // aplicación de una vacuna para producir inmunidad activa o pasiva
+                        paciente,
+                        confidencialidad: 'N',
+                        profesional: {
+                            nombre: '-',
+                            apellido: '-'
+                        },
+                        cie10: 'Z26.9', // CIE10: Vacunación profilactica
+                        file: null,
+                        texto: texto + `Vacuna: ${vacunas[i].sniVacunaNombre} Dosis: ${vacunas[i].sniDosisNombre || ''} Esquema: ${vacunas[i].vacunaEsquemaNombre} pertenece al lote: ${vacunas[i].lote}`
+                    };
+                    promesas.push(operations.postCDA(dto));
 
-            let promesas = [];
-            for (let i = 0; i < vacunas.length; i++) {
-                let texto = vacunas[i].origenNombre ? `Organización:  ${vacunas[i].origenNombre} / ` : '';
-                const dto = {
-                    id: vacunas[i].idSniAplicacion.toString(), // codigo SISA de la vacuna
-                    organizacion: organizacionId,
-                    fecha: vacunas[i].fechaAplicacion,  // Fecha de aplicación de la dosis de vacuna.
-                    tipoPrestacion: '33879002', // aplicación de una vacuna para producir inmunidad activa o pasiva
-                    paciente,
-                    confidencialidad: 'N',
-                    profesional: {
-                        nombre: '-',
-                        apellido: '-'
-                    },
-                    cie10: 'Z26.9', // CIE10: Vacunación profilactica
-                    file: null,
-                    texto: texto + `Vacuna: ${vacunas[i].sniVacunaNombre} Dosis: ${vacunas[i].sniDosisNombre || ''} Esquema: ${vacunas[i].vacunaEsquemaNombre} pertenece al lote: ${vacunas[i].lote}`
-                };
-                promesas.push(operations.postCDA(dto));
-
-                const dtoMongoDB = {
-                    idvacuna: vacunas[i].idSniAplicacion.toString(),
-                    codigo: vacunas[i].idSniVacuna.toString() || '',
-                    documento: vacunas[i].nroDoc,
-                    apellido: vacunas[i].apellido,
-                    nombre: vacunas[i].nombre,
-                    fechaNacimiento: vacunas[i].fechaNacimiento,
-                    sexo: paciente.sexo,
-                    vacuna: vacunas[i].sniVacunaNombre,
-                    dosis: vacunas[i].sniDosisNombre,
-                    fechaAplicacion: vacunas[i].fechaAplicacion,
-                    efector: vacunas[i].origenNombre,
-                    esquema: vacunas[i].vacunaEsquemaNombre || '',
-                    condicion: vacunas[i].sniAplicacionCondicionNombre
-                };
-                promesas.push(operations.postMongoDB(dtoMongoDB));
+                    const dtoMongoDB = {
+                        idvacuna: vacunas[i].idSniAplicacion.toString(),
+                        codigo: vacunas[i].idSniVacuna.toString() || '',
+                        documento: vacunas[i].nroDoc,
+                        apellido: vacunas[i].apellido,
+                        nombre: vacunas[i].nombre,
+                        fechaNacimiento: vacunas[i].fechaNacimiento,
+                        sexo: paciente.sexo,
+                        vacuna: vacunas[i].sniVacunaNombre,
+                        dosis: vacunas[i].sniDosisNombre,
+                        fechaAplicacion: vacunas[i].fechaAplicacion,
+                        efector: vacunas[i].origenNombre,
+                        esquema: vacunas[i].vacunaEsquemaNombre || '',
+                        condicion: vacunas[i].sniAplicacionCondicionNombre
+                    };
+                    promesas.push(operations.postMongoDB(dtoMongoDB));
+                }
+                await Promise.all(promesas);
+            } else {
+                return null;
             }
-            await Promise.all(promesas);
         } catch (e) {
             let fakeRequest = {
                 user: {
