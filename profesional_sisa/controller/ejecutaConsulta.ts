f6a90e581bab715527ccb6a787571d62abc61dfd
plammel <lammelpablo@gmail.com>
2021-02-23 16:09:58 -0300

    feat: export profesional SISA (#114)
    * feat: export profesional SISA
    
    * fix(profesional-sisa): cambia formato fecha
    
    Co-authored-by: Natalia Huenchuman <natalia.huenchuman@gmail.com>

---
+++ profesional_sisa/controller/ejecutaConsulta.ts
@@ -0,0 +1,20 @@
+import { postProfesionalSISA, crearProfesionalSISA } from '../service/operaciones.service';
+
+
+export async function exportSISA(profesional) {
+    let postsSISA = [];
+
+    if(isMatriculado(profesional)) {
+        let formacionesGrado = profesional.formacionGrado.filter(async e => e.matriculado);
+        for (let formacionGrado of formacionesGrado) {
+            let profesionalSisa = await crearProfesionalSISA(profesional, formacionGrado);
+            postsSISA.push(postProfesionalSISA(profesionalSisa));
+        }
+    }
+
+    return await Promise.all(postsSISA);
+}
+
+function isMatriculado(profesional) {
+    return profesional.formacionGrado && profesional.formacionGrado.length && profesional.formacionGrado.find(e => e.matriculado);
+}
