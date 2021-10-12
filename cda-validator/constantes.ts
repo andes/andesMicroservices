741044f6a9562e0ed19675c578f7d6fc4d50d7d3
silviroa <silviroa@gmail.com>
2021-10-12 17:18:39 -0300

    fix: Quitar la configuracion de Heller para consultar cdas

--- cda-validator/constantes.ts
+++ cda-validator/constantes.ts
@@ -1,1 +1,1 @@
-export const efectores = ['hpn', 'heller', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet', 'senillosa', 'picun', 'piedra', 'chanar', 'rincon', 'villa_angostura', 'chosmalal', 'cutral_co'];
+export const efectores = ['hpn', 'sips', 'junin', 'plottier', 'zapala', 'centenario', 'bouquet', 'senillosa', 'picun', 'piedra', 'chanar', 'rincon', 'villa_angostura', 'chosmalal', 'cutral_co'];
