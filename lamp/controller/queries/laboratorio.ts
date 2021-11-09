195562f998f4bebebe3d0465eb126e8672e76a26
plammel <lammelpablo@gmail.com>
2021-11-09 14:40:46 -0300

    EP: chunck array documentos

--- lamp/controller/queries/laboratorio.ts
+++ lamp/controller/queries/laboratorio.ts
@@ -7,7 +7,7 @@ export function make(pacientes, fechaTope) {
         server: config.conSql.serverSql.server,
         database: config.conSql.serverSql.database,
         options: {
-            encrypt: true
+            tdsVersion: '7_1'
         }
     };
     const codigoLAMP = '9063'
