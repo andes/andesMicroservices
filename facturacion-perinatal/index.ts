5a3e8e15e32aabd475dfc69dfef2f9fe239d392a
mcele <MCele>
2022-03-11 16:38:46 -0300

    fix(Facturracion): agrega control para no mapear cuando sólo se registre una prestación con un número de embarazo

--- facturacion-perinatal/index.ts
+++ facturacion-perinatal/index.ts
@@ -1,7 +1,7 @@
 import { Microservice } from '@andes/bootstrap';
 import { Connections } from '@andes/log';
 import { logDatabase } from './config.private';
-import { createFacturarPerinatal, updateFacturarPerinatal } from './controller/perinatal';
+import { createFacturarPerinatal, updateFacturarPerinatal, getRegistros } from './controller/perinatal';
 
 let pkg = require('./package.json');
 let ms = new Microservice(pkg);
@@ -15,15 +15,29 @@ router.group('/perinatalSql', (group) => {
         res.send({ message: 'ok' });
         try {
             const prestacion = req.body.data.prestacion;
-            Connections.initialize(logDatabase.log.host, logDatabase.log.options);
-            const cantValidaciones = prestacion.estados.filter(est => est.tipo === 'ejecucion').length;
-            if (cantValidaciones < 2) {
-                //  si la prestación solo se validó una vez, entonces la creamos en sips
-                await createFacturarPerinatal(prestacion);
+            const conceptConsultaEmbarazo = ['1191000013107', '1141000013103',
+                '1341000013100', '1201000013105', '1741000013104', '4501000013105', '1711000013103'];
+            const conceptId = prestacion.solicitud.tipoPrestacion.conceptId;
+            // verificamos si la prestación es algunas de las relacionadas a control de embarazo
+            // si no lo es, entonces verificamos que contenga entre sus registros alguno estos conceptos
+            let enviarFacturacion: boolean = conceptConsultaEmbarazo.includes(conceptId);
+
+            if (!enviarFacturacion) {
+                const registros = getRegistros(prestacion.ejecucion.registros);
+                // Cuando se registra concepto número de "embarazo" de cualquier otra prestación.
+                enviarFacturacion = registros.find(reg => conceptConsultaEmbarazo.includes(reg.conceptId));
             }
-            else {
-                // si la consulta es revalidada
-                await updateFacturarPerinatal(prestacion);
+            if (enviarFacturacion) {
+                Connections.initialize(logDatabase.log.host, logDatabase.log.options);
+                const cantValidaciones = prestacion.estados.filter(est => est.tipo === 'ejecucion').length;
+                if (cantValidaciones < 2) {
+                    //  si la prestación solo se validó una vez, entonces la creamos en sips
+                    await createFacturarPerinatal(prestacion);
+                }
+                else {
+                    // si la consulta es revalidada
+                    await updateFacturarPerinatal(prestacion);
+                }
             }
         } catch (e) {
             throw e;
--- sip-plus-perinatal/index.ts
+++ sip-plus-perinatal/index.ts
@@ -1,8 +1,9 @@
 import { Microservice } from '@andes/bootstrap';
 import { Connections } from '@andes/log';
-import { logDatabase } from './config.private';
-import { getPaciente as getPacienteSP, postPaciente as postPacienteSP } from './controller/sip-plus';
+import { logDatabase, fakeRequest } from './config.private';
+import { getPaciente as getPacienteSP, postPaciente as postPacienteSP, getRegistros } from './controller/sip-plus';
 import { getPaciente as getPacienteAndes } from './service/paciente';
+import { log } from '@andes/log';
 
 let pkg = require('./package.json');
 let ms = new Microservice(pkg);
@@ -16,17 +17,31 @@ router.group('/perinatal', (group) => {
         res.send({ message: 'ok' });
         try {
             const prestacion = req.body.data.prestacion;
-            let paciente: any = await getPacienteAndes(prestacion.paciente.id);
-            if (paciente) {
-                // Obtenemos el paciente y sus gestas cargadas en sip plus
-                const resultSP = await getPacienteSP(paciente);
-                if (resultSP) {
-                    // peticion válida
-                    await postPacienteSP(paciente, prestacion, resultSP.paciente || {});
+            // Se asume que el registro de número de "embarazo" está incluido en todos los conceptConsultaEmbarazo.
+            const conceptConsultaEmbarazo = ['1191000013107', '1141000013103',
+                '1341000013100', '1201000013105', '1741000013104', '4501000013105', '1711000013103'];
+            const conceptId = prestacion.solicitud.tipoPrestacion.conceptId;
+            // verificamos si la prestación es algunas de las relacionadas a control de embarazo
+            // si no lo es, entonces verificamos que contenga entre sus registros alguno de estos conceptos
+            let mapeoSipPlus: boolean = conceptConsultaEmbarazo.includes(conceptId);
+            const registros = getRegistros(prestacion.ejecucion.registros);
+            if (!mapeoSipPlus) {
+                // Se verifica que la prestación incluya algún registro relacionado a control de embarazo
+                mapeoSipPlus = registros.find(reg => conceptConsultaEmbarazo.includes(reg.concepto.conceptId));
+            }
+            if (mapeoSipPlus) {
+                let paciente: any = await getPacienteAndes(prestacion.paciente.id);
+                if (paciente) {
+                    // Obtenemos el paciente y sus gestas cargadas en sip plus
+                    const resultSP = await getPacienteSP(paciente);
+                    if (resultSP) {
+                        // peticion válida
+                        await postPacienteSP(paciente, prestacion, registros, resultSP.paciente || {});
+                    }
                 }
             }
         } catch (e) {
-            throw e;
+            log(fakeRequest, 'microservices:integration:sip-plus', { data: req.body.data }, 'postInitial:register', null, null, e);
         }
     });
 
