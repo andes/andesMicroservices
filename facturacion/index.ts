fb26f7fec2a883281fb1e4c67a9d3ec1bf1ec972
plammel <lammelpablo@gmail.com>
2022-07-15 09:22:04 -0300

    FAC(fix): control de concurrencia y cierre de conexiones (#196)

--- facturacion/index.ts
+++ facturacion/index.ts
@@ -18,38 +18,46 @@ router.group('/facturacion', (group) => {
     Connections.initialize(logDatabase.log.host, logDatabase.log.options);
 
     group.post('/facturar', async (req, res) => {
-        const data = req.body.data;
-        let dtoFacturacion: any;
-        try {
-            sql.close();
-            let pool = await sql.connect(SipsDBConfiguration);
-            const event = req.body.event;
-
-            switch (event) {
-                case 'facturacion:factura:buscador':
-                    dtoFacturacion = await facturaBuscador(data);
-                    break;
-                case 'facturacion:factura:recupero_financiero':
-                    dtoFacturacion = await facturaTurno(data);
-                    break;
-                /* Queda comentado para cuando se habilite la facturación desde RUP*/
-                // case 'rup:prestacion:validate':
-                //     dtoFacturacion = await facturaRup(data);
-                //     break;
-                default:
-                    log.error('facturacion:create', { event, data }, 'Origen facturación inválido', userScheduler);
-                    break;
-            }
-
-            let factura = new Factura();
-            for (let x = 0; x < dtoFacturacion.length; x++) {
-                await factura.facturar(pool, dtoFacturacion[x]);
-            }
-        } catch (error) {
-            log.error('facturacion:create:error', { data, dtoFacturacion }, error, userScheduler);
-        }
-        sql.close();
         res.json('OK');
+        let _pool;
+        new sql.ConnectionPool(SipsDBConfiguration)
+            .connect()
+            .then( async (pool) => {
+                _pool = pool;
+                const data = req.body.data;
+                let dtoFacturacion: any;
+        
+                const event = req.body.event;
+
+                switch (event) {
+                    case 'facturacion:factura:buscador':
+                        dtoFacturacion = await facturaBuscador(data);
+                        break;
+                    case 'facturacion:factura:recupero_financiero':
+                        dtoFacturacion = await facturaTurno(data);
+                        break;
+                    /* Queda comentado para cuando se habilite la facturación desde RUP*/
+                    // case 'rup:prestacion:validate':
+                    //     dtoFacturacion = await facturaRup(data);
+                    //     break;
+                    default:
+                        log.error('facturacion:create', { event, data }, 'Origen facturación inválido', userScheduler);
+                        break;
+                }
+    
+                let factura = new Factura();
+                for (let x = 0; x < dtoFacturacion.length; x++) {
+                    await factura.facturar(pool, dtoFacturacion[x]);
+                }
+                pool.close();
+    
+            }).catch(error => {
+                // PATCH: en caso ocurrir una excepción no handleada y quedar abierta la conexión
+                if (_pool?._connected) {
+                    _pool.close();
+                }
+                return log.error('facturacion:create:error', { data: req.body, error }, userScheduler);
+            });
     });
 });
 
