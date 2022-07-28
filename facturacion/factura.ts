57462cb88a66649a50d4bff536fbf56c65057857
plammel <lammelpablo@gmail.com>
2022-07-28 08:57:12 -0300

    FAC(feat): query recupero refactor (#197)

--- facturacion/factura.ts
+++
@@ -1,9 +0,0 @@
-import { jsonFacturacion } from './configJson/configJson';
-import { IDtoFacturacion } from './interfaces/IDtoFacturacion';
-
-export class Factura {
-
-    async facturar(pool: any, dtoFacturacion: IDtoFacturacion) {
-        await jsonFacturacion(pool, dtoFacturacion);
-    }
-}
